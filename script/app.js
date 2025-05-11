789d61af659a329c0cbc9e5a0b0a43662762dc76
Rserli <riccardoserli.95@gmail.com>
2025-05-11 20:56:39 +0200

    Check on outputs

--- script/app.js
+++ script/app.js
@@ -13,13 +13,28 @@ let NumeroIncrem // number
 for (let i = 0; i < 100; i++) {
     NumeroIncrem = i + 1
     console.log(`Ciclo=${i} NumIncrem=${NumeroIncrem}`)
-    //  calcolo del modulo del primo divisore
+    //  calcolo del resto con il primo divisore
     let Result_FirtModule = NumeroIncrem % PRIMO_DIVISORE //numeber
     console.log("Moudulo di " + PRIMO_DIVISORE + " = " + Result_FirtModule)
-    //  calcolo del modulo del secondo divisore
+    //  calcolo del rsto  con il secondo divisore
     let Result_SecondModule = NumeroIncrem % SECONDO_DIVISORE //number
     console.log("Moudulo di " + SECONDO_DIVISORE + " = " + Result_SecondModule)
-    //  CONTROLLO sull'output
-
+    // --- CONTROLLO sull'output ---
+    // SE il resto è 0 con il primo divisore restitusce --> Fizz
+    if (Result_FirtModule == 0 && Result_SecondModule != 0) {
+        console.log("Fizz")
+    }
+    // SE il resto è 0 con il secondo divisore restitusce --> Buzz
+    else if (Result_FirtModule != 0 && Result_SecondModule == 0) {
+        console.log("Buzz")
+    }
+    // SE il resto è 0 con il primo divisore sia con il secondo restitusce --> FizzBuzz
+    else if (Result_FirtModule == 0 && Result_SecondModule == 0) {
+        console.log("FizzBuzz")
+    }
+    // ALTRIMENTI restituisce solo il numero
+    else {
+        console.log(NumeroIncrem)
+    }
     console.log("--------------------------")
 }
