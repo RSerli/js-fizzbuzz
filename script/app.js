e8084d38eeed0ab4bfdb2aaae7c0e6c5bfd19822
Rserli <riccardoserli.95@gmail.com>
2025-05-11 20:02:11 +0200

    Human Coding

---
+++ script/app.js
@@ -0,0 +1,12 @@
+console.log("FuzzBuzz")
+
+// ---  VARIABILI ---
+// Primo Divisore
+// Secondo Dividsore
+// Numero Incrementale
+
+// --- MAIN ---
+// Inizio Ciclo per conteggio
+//  calcolo del modulo del primo divisore
+//  calcolo del modulo del secondo divisore
+//  CONTROLLO sull'output
